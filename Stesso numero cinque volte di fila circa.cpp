e63b9a2449929d024c8dd2301a20cb14111a2c04
Solo un tostapane <33027100+JustAToaster@users.noreply.github.com>
2017-11-19 15:38:56 +0100

    Cacca
    Al forno

---
+++ Stesso numero cinque volte di fila circa.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include<cstdlib>
+
+#include<time.h>
+
+#include<stdlib.h>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+   int dado = 0; //il dado e basta.
+   
+   int dado1 = 0; //variabile a cui assegno il numero del lancio precedente.
+
+   int y = 0; //Numero di volte per cui il numero appare di seguito.
+
+   int lanci = 0; //i lanci lmao
+
+   
+
+   cout << " Questo programma lancia un dado numerose volte, finche' non ottiene cinque num. uguali di seguito.";
+
+   cout << "\n Sotto vedrai riportato il numero di lanci effettuati.\n";
+
+   srand(time(0));
+
+    
+
+   while (y < 1)
+
+      {
+
+        int aux = rand();
+
+        dado = (aux % 6) + 1;
+
+        cout<<" "<<dado<<" ";
+        
+        if (dado==dado1)
+        { 
+        	y++;
+        }
+    	else 
+		{
+        	y=0;
+		} 
+		dado = dado1;
+        lanci++; 
+      } 
+
+      
+
+   cout << "\nLanci effettuati : " << lanci << endl; 
+
+
+   system("PAUSE"); // ns. comodo!
+
+   return 0;
+
+} // end main()
+
+    
